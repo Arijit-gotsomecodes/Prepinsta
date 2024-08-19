c875a19ad901b5e9ebb9db4f3b2dcb6f22ff4a03
Arijit-gotsomecodes <arijit.deb1@s.amity.edu>
2024-08-19 18:03:29 +0530

    updated week 7

---
+++ Week 7 Task/Add-to-cart/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import CartContextProvider from "./store/shopping-cart-context";
+
+function App() {
+  return (
+    <CartContextProvider>
+      <Header />
+      <Shop />
+    </CartContextProvider>
+  );
+}
+
+export default App;
---
+++ Week 7 Task/Add-to-cart/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { useRef } from "react";
+
+import CartModal from "./CartModal.jsx";
+
+import { CartContext } from "../store/shopping-cart-context";
+
+import { useContext } from "react";
+
+export default function Header({ cart, onUpdateCartItemQuantity }) {
+  const { items } = useContext(CartContext);
+  const modal = useRef();
+
+  const cartQuantity = items.length;
+
+  function handleOpenCartClick() {
+    modal.current.open();
+  }
+
+  let modalActions = <button>Close</button>;
+
+  if (cartQuantity > 0) {
+    modalActions = (
+      <>
+        <button>Close</button>
+        <button>Checkout</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <CartModal ref={modal} title="Your Cart" actions={modalActions} />
+      <header id="main-header">
+        <div id="main-title">
+          <img src="logo.png" alt="Elegant model" />
+          <h1>Elan Vintage</h1>
+        </div>
+        <p>
+          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
+        </p>
+      </header>
+    </>
+  );
+}
